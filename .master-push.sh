bc43537ebd391e1b1a44eae9513658ef1e903381
Benjamin Feng <contact@fengb.me>
2017-12-28 20:13:13 -0600

    Fix Gitlab upload

---
+++ .master-push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+git checkout -q master
+cp hosts.new hosts
+if git diff --exit-code; then
+  exit
+fi
+
+git config user.name "CI"
+git config user.email "ci@example.com"
+git commit hosts -q --message='Autocommit'
+git push -q "$(sed -e "s#//.*@#//oauth2:$ACCESS_TOKEN@#" <<<"$CI_REPOSITORY_URL")" master
