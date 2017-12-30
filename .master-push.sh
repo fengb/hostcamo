2ddfe9acb511cb24036d9684eb88af3404a45910
Benjamin Feng <contact@fengb.me>
2017-12-29 22:13:22 -0600

    QUIET

--- .master-push.sh
+++ .master-push.sh
@@ -3,13 +3,13 @@
 set -euo pipefail
 
 mv hosts hosts.new
-git checkout -q master
+git checkout --quiet master
 mv hosts.new hosts
-if git diff -q --exit-code; then
+if git diff --quiet --exit-code; then
   exit
 fi
 
 git config user.name "CI"
 git config user.email "ci@example.com"
-git commit hosts -q --message='Autocommit'
-git push -q "$(sed -e "s#//.*@#//oauth2:$ACCESS_TOKEN@#" <<<"$CI_REPOSITORY_URL")" master
+git commit hosts --quiet --message='Autocommit'
+git push --quiet "$(sed -e "s#//.*@#//oauth2:$ACCESS_TOKEN@#" <<<"$CI_REPOSITORY_URL")" master
