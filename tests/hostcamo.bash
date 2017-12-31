eea7c5343cdbc87ddee1900c433fe3a10da11c86
Benjamin Feng <contact@fengb.me>
2017-12-31 00:27:32 -0600

    Added tests

---
+++ tests/hostcamo.bash
@@ -0,0 +1,30 @@
+source "$(dirname "$BASH_SOURCE")/../hostcamo"
+
+function test_strip {
+  assert_equals 'localhost' \
+    "$(strip <<<'127.0.0.1 localhost')"
+  assert_equals 'broadcast' \
+    "$(strip <<<'255.255.255.255 broadcast')"
+}
+
+function test_filter_valid_domains {
+  assert_equals '' \
+    "$(filter <<<$'\n\n\n')"
+  assert_equals 'realdomain.com' \
+    "$(filter <<<'realdomain.com')"
+  assert_equals 'xn--oogle-wmc.com' \
+    "$(filter <<<'xn--oogle-wmc.com')"
+  assert_equals '' \
+    "$(filter <<<'ɢoogle.com')"
+}
+
+function test_filter_whitelist {
+  assert_equals '' \
+    "$(filter <<<'localhost')"
+  assert_equals '' \
+    "$(filter <<<'localhost.localdomain')"
+  assert_equals '' \
+    "$(filter <<<'0.0.0.0')"
+  assert_equals '0-0-0-0' \
+    "$(filter <<<'0-0-0-0')"
+}
