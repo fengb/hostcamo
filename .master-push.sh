26375f48537443500e09be340491677e08c1764e
Benjamin Feng <contact@fengb.me>
2018-02-06 17:08:23 -0600

    Shorten auto commit message

--- .master-push.sh
+++ .master-push.sh
@@ -11,5 +11,5 @@ fi
 
 git config user.name "CI"
 git config user.email "ci@example.com"
-git commit hosts --quiet --message="Autocommit from $CI_COMMIT_SHA"
+git commit hosts --quiet --message="Autocommit - ${CI_COMMIT_SHA:0:8}"
 git push --quiet "$(sed -e "s#//.*@#//oauth2:$ACCESS_TOKEN@#" <<<"$CI_REPOSITORY_URL")" master
