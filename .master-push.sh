a56e38ed1b7476e30522a40aec7284c6764b2d13
Benjamin Feng <contact@fengb.me>
2018-03-16 11:23:44 -0500

    Pull variable declarations to top

--- .master-push.sh
+++ .master-push.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+COMMIT_MESSAGE="[CI][${CI_PIPELINE_SOURCE}] ${CI_COMMIT_SHA:0:8}"
+SECURE_URL="${CI_REPOSITORY_URL/\/\/*@///oauth:$ACCESS_TOKEN@}"
+
 mv hosts hosts.new
 git checkout --quiet master
 mv hosts.new hosts
@@ -11,6 +14,5 @@ fi
 
 git config user.name "CI"
 git config user.email "ci@example.com"
-git commit hosts --quiet --message="[CI][${CI_PIPELINE_SOURCE}] ${CI_COMMIT_SHA:0:8}"
-SECURE_URL="$(sed -e "s#//.*@#//oauth2:$ACCESS_TOKEN@#" <<<"$CI_REPOSITORY_URL")"
+git commit hosts --quiet --message="$COMMIT_MESSAGE"
 git push --quiet "$SECURE_URL" master
