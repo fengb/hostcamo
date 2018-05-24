8332cd26f48a8f12e850a8bc7e8c948be99ac23b
Benjamin Feng <contact@fengb.me>
2018-05-24 15:13:18 -0500

    Generate intermediate files

--- tests/hostcamo.bash
+++ tests/hostcamo.bash
@@ -1,5 +1,14 @@
 source "$(dirname "$BASH_SOURCE")/../hostcamo"
 
+function test_as_filename {
+  assert_equals 'google.com' \
+    "$(as_filename http://google.com)"
+  assert_equals 'google.com' \
+    "$(as_filename https://google.com)"
+  assert_equals 'google.com--banana' \
+    "$(as_filename https://google.com/banana)"
+}
+
 function test_strip {
   assert_equals 'localhost' \
     "$(strip <<<'127.0.0.1 localhost')"
